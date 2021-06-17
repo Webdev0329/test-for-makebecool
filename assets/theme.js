fc10daf10fd7900384295b02c33c72bdae2cadb5
Webdev0329 <wstar066@gmail.com>
2021-06-17 04:55:10 -0400

    product page done

--- assets/theme.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-	$(".btn-hamburger").click(function(e) {
-		$(".mobile-nav").addClass("opened");
-	});
-
-	$(".mobile-nav .btn-close").click(function(e) {
-		$(".mobile-nav").removeClass("opened");
-	});
-});
